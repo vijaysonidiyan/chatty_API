c60d760baf560b5bac75da6056efbc941f9e0507
Shweta Soni <shwetasoni@diyantech.in>
2022-12-20 16:07:01 +0530

    changes Done

--- server/web/middleware.js
+++ server/web/middleware.js
@@ -11,6 +11,7 @@ const CONFIG = require("./../config");
 const multer = require("multer");
 const { expressjwt: jwt } = require('express-jwt');
 const blacklist = require("express-jwt-blacklist");
+const fs = require("fs");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     let dirPath = CONFIG.UPLOADS.DEFAULT;
@@ -23,6 +24,17 @@ const storage = multer.diskStorage({
         file.fieldname === "Image"
       ) {
         dirPath = CONFIG.UPLOADS.DIR_PATH_PHOTOS;
+        if (!!dirPath) {
+
+          var path = CONFIG.UPLOADS.DIR_PATH_PHOTOS;
+
+          if (!fs.existsSync(path)) {
+
+            fs.mkdirSync(path, { recursive: true });
+
+          }
+
+        }
       } else if (
         file.fieldname === "videos" ||
         file.fieldname === "function_video"
@@ -49,12 +61,12 @@ const upload = multer({
 module.exports = (app) => {
   app.use(compression());
   app.use(cors());
- 
+
   CONFIG.JWTTOKENALLOWACCESS = jwt({
     secret: CONFIG.JWTTOKENKEY,
     algorithms: ['HS256'],
     userProperty: "payload",
- //   isRevoked: blacklist.isRevoked,
+    //   isRevoked: blacklist.isRevoked,
   });
   app.use(bodyParser.json({ limit: "2gb" }));
   app.use(
@@ -144,7 +156,7 @@ module.exports = (app) => {
   app.use("/v1/user", cpUpload, require("./routes/v1/user"));
   app.use("/v1/chat", cpUpload, require("./routes/v1/chat"));
   app.use("/v1/blockUser", cpUpload, require("./routes/v1/blockUser"));
-  
+
   app.get(
     /^\/(?!.*assets\/.*|.*resize\/.*|.*vendor\/.*|.*css\/.*|.*admin\/.*).*$/,
     function (req, res) {
@@ -152,7 +164,7 @@ module.exports = (app) => {
       res.sendFile(path.resolve(__dirname + "/index.html"));
     }
   );
-  
+
   app.use((req, res, next) => {
     next(createError(404));
   });
