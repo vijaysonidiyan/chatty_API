7444982ca270692d05a627e77756ac14f3b71595
Shweta Soni <shwetasoni@diyantech.in>
2023-02-03 14:48:09 +0530

    chatmodel

--- server/common/model/chatModel.js
+++ server/common/model/chatModel.js
@@ -9,6 +9,8 @@ class chatModel extends ModelBase {
             message: { type: String, allowNullEmpty: true },
             file_name: { type: String, allowNullEmpty: true },
             video_screenshort:{type: String, allowNullEmpty: true},
+            thumbnail:{type: String, allowNullEmpty: true},
+            size:{type:String,allowNullEmpty: true },
             sender_id: { type: Object, allowNullEmpty: true },
             reciver_id: { type: Object, allowNullEmpty: true },
             postId: { type: Object, allowNullEmpty: true },
