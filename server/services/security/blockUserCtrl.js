037452b2ef3e5e50a85f8733c216f710a4fe5760
Shweta Soni <shwetasoni@diyantech.in>
2023-02-13 17:05:43 +0530

    index.js block_user emitter set , blockUser , unblockUser

--- server/services/security/blockUserCtrl.js
+++ server/services/security/blockUserCtrl.js
@@ -25,7 +25,7 @@ blockUserCtrl.blockUser = (req, res) => {
   try {
     BlockUserModel.findOne(
       {
-        userId: ObjectID(req.body._id),
+        userId: ObjectID(req.auth._id),
         blockedUserId: ObjectID(req.body.blockedUserId),
         status: 1,
       },
@@ -37,7 +37,7 @@ blockUserCtrl.blockUser = (req, res) => {
 
           BlockUserModel.create(
             {
-              userId: ObjectID(req.body._id),
+              userId: ObjectID(req.auth._id),
               blockedUserId: ObjectID(req.body.blockedUserId),
             },
             (err, blockedUser) => {
@@ -47,91 +47,91 @@ blockUserCtrl.blockUser = (req, res) => {
               } else {
 
                 //block array save in user table 
-                UserModel.findOne(
-                  {
-                    _id: ObjectID(req.body._id),
-                  },
-                  (err, userfind) => {
-                    if (err) {
-                      console.log(err);
-                      throw err;
-                    } else {
-                      if (userfind == null) {
-                        AppCode.Fail.error = "No record found";
-                        response.setError(AppCode.Fail);
-                        response.send(res);
-                      } else {
+                // UserModel.findOne(
+                //   {
+                //     _id: ObjectID(req.auth._id),
+                //   },
+                //   (err, userfind) => {
+                //     if (err) {
+                //       console.log(err);
+                //       throw err;
+                //     } else {
+                //       if (userfind == null) {
+                //         AppCode.Fail.error = "No record found";
+                //         response.setError(AppCode.Fail);
+                //         response.send(res);
+                //       } else {
 
-                        if (!!userfind.blockUser) {
-                          let updateDataQuery = {};
-                          console.log("................", userfind.blockUser)
-                          let aaa = []
-                          let bbb = []
-                          aaa = userfind.blockUser
-                          console.log(",,,,,,,,,,,,,,,,,,,,,,,", req.body.blockedUserId)
-                          bbb.push(ObjectID(req.body.blockedUserId))
+                //         if (!!userfind.blockUser) {
+                //           let updateDataQuery = {};
+                //           console.log("................", userfind.blockUser)
+                //           let aaa = []
+                //           let bbb = []
+                //           aaa = userfind.blockUser
+                //           console.log(",,,,,,,,,,,,,,,,,,,,,,,", req.body.blockedUserId)
+                //           bbb.push(ObjectID(req.body.blockedUserId))
 
-                          let abc = aaa.concat(bbb)
-                          console.log("......................................abcccccccc", abc)
-
-
-                          console.log(".........abc after", abc)
+                //           let abc = aaa.concat(bbb)
+                //           console.log("......................................abcccccccc", abc)
 
 
-                          updateDataQuery.blockUser = abc
-                          console.log(".......updateDataQuery.......", updateDataQuery)
-
-                          // delete req.body._id
-                          UserModel.update({ _id: ObjectID(req.body._id) }, updateDataQuery, function (err, roleUpdate) {
-                            if (err) {
-                              console.log("...........", err);
-                              response.setError(AppCode.Fail);
-                            } else {
-
-                              console.log(")))))))))", updateDataQuery);
-                              response.setData(AppCode.Success);
-                              response.send(res);
-                            }
-                          });
+                //           console.log(".........abc after", abc)
 
 
+                //           updateDataQuery.blockUser = abc
+                //           console.log(".......updateDataQuery.......", updateDataQuery)
 
-                        }
-                        else {
-                          let updateDataQuery = {};
+                //           // delete req.body._id
+                //           UserModel.update({ _id: ObjectID(req.auth._id) }, updateDataQuery, function (err, roleUpdate) {
+                //             if (err) {
+                //               console.log("...........", err);
+                //               response.setError(AppCode.Fail);
+                //             } else {
 
-
-                          let a1 = []
-                          a1.push(ObjectID(req.body.blockedUserId))
-
-                          console.log("a111111111111111111", a1);
-                          updateDataQuery.blockUser = a1
-
-                          console.log("<MMMMMMMM", updateDataQuery);
+                //               console.log(")))))))))", updateDataQuery);
+                //               response.setData(AppCode.Success);
+                //               response.send(res);
+                //             }
+                //           });
 
 
 
-                          // delete req.body._id
-                          UserModel.update({ _id: ObjectID(req.body._id) }, updateDataQuery, function (err, roleUpdate) {
-                            if (err) {
-                              console.log("...........", err);
-                              response.setError(AppCode.Fail);
-                            } else {
+                //         }
+                //         else {
+                //           let updateDataQuery = {};
 
-                              console.log(")))))))))", updateDataQuery);
-                              response.setData(AppCode.Success);
-                              response.send(res);
-                            }
-                          });
 
-                        }
+                //           let a1 = []
+                //           a1.push(ObjectID(req.body.blockedUserId))
 
-                      }
-                    }
-                  }
-                );
-                // response.setError(AppCode.Success, blockedUser);
-                // response.send(res);
+                //           console.log("a111111111111111111", a1);
+                //           updateDataQuery.blockUser = a1
+
+                //           console.log("<MMMMMMMM", updateDataQuery);
+
+
+
+                //           // delete req.body._id
+                //           UserModel.update({ _id: ObjectID(req.auth._id) }, updateDataQuery, function (err, roleUpdate) {
+                //             if (err) {
+                //               console.log("...........", err);
+                //               response.setError(AppCode.Fail);
+                //             } else {
+
+                //               console.log(")))))))))", updateDataQuery);
+                //               response.setData(AppCode.Success);
+                //               response.send(res);
+                //             }
+                //           });
+
+                //         }
+
+                //       }
+                //     }
+                //   }
+                // );
+                 response.setError(AppCode.Success, blockedUser);
+                 response.send(res);
 
               }
             }
@@ -148,162 +148,6 @@ blockUserCtrl.blockUser = (req, res) => {
     response.setError(AppCode.InternalServerError);
     response.send(res);
   }
-};
-
-blockUserCtrl.getBlockUserList = (req, res) => {
-  const response = new HttpRespose();
-  let data = req.body;
-  let searchKey = "";
-  let options = {};
-  searchKey = !!req.query.searchKey ? req.query.searchKey : "";
-  let pageNumber = !!req.query.pageNumber ? req.query.pageNumber : 0;
-  // let loginUserId = ObjectID(req.payload.userId);
-  const limit = 10;
-  const skip = limit * parseInt(pageNumber);
-  options.skip = skip;
-  options.limit = limit;
-  getBlockedUserList(req.auth._id).then((user) => {
-    console.log(",,,,,,,,,,", user)
-    const blockuser = [];
-    _.forEach(user.blockedUserData, (follower) => {
-      console.log("------follower-----------------", follower);
-      blockuser.push(ObjectID(follower.blockedUserId));
-      console.log("---------buddyRequest--------------", blockuser);
-    });
-
-    let query = [
-      {
-        $match: {
-          $or: [
-            {
-              //userName: new RegExp('^' + searchKey.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'i'),
-              userName: new RegExp(
-                ".*" + searchKey.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&") + ".*",
-                "i"
-              ),
-            },
-
-          ],
-
-          $expr: {
-            $in: ["$_id", blockuser],
-          },
-          // status: { $ne: 2 },
-          // isdeleted: { $ne: true },
-        },
-      },
-      { $sort: { userName: 1 } },
-      { $skip: skip },
-      { $limit: limit },
-      {
-        $project: {
-          _id: 1,
-          userId: 1,
-          userName: 1,
-         
-          profile_image: { $ifNull: ["$profile_image", ""] },
-
-          // qualityRating: { $ifNull: ["$qualityRating", 0] },
-          // rolies: { $ifNull: ["$roliesRating", 0] },
-          // statusType: 1,
-          // quntities: { $ifNull: ["$quntitiesRating", 0] },
-          // Rating: {
-          //   $ifNull: [
-          //     {
-          //       $divide: [
-          //         {
-          //           $sum: [
-          //             "$qualityRating",
-          //             "$roliesRating",
-          //             "$quntitiesRating",
-          //           ],
-          //         },
-          //         3,
-          //       ],
-          //     },
-          //     0,
-          //   ],
-          // },
-          //  about: 1,
-          // isBuddy: "false",
-          //  isBuddyRequested: "false",
-        },
-      },
-    ];
-    let countQuery = {
-      $or: [
-        {
-          userName: new RegExp(
-            ".*" + searchKey.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&") + ".*",
-            "i"
-          ),
-        },
-      ],
-
-      $expr: {
-        $in: ["$_id", blockuser],
-      },
-    }
-    try {
-      let result = {};
-      async.parallel(
-        [
-          function (cb) {
-            //UserModel.advancedAggregate(query, {}, (err, countData) => {
-            UserModel.count(countQuery, (err, countData) => {
-              if (err) {
-                throw err;
-              } else if (options.skip === 0 && countData === 0) {
-                cb(null);
-              } else if (options.skip > 0 && countData === 0) {
-                cb(null);
-              } else {
-                console.log("....coundata", countData)
-                if (countData <= skip + limit) {
-                  result.totalblockuser = countData;
-                } else {
-                  result.nextPage = parseInt(pageNumber) + 1;
-                  result.totalblockuser = countData;
-                }
-                cb(null);
-              }
-            });
-          },
-          function (cb) {
-            UserModel.aggregate(query, (err, followers) => {
-              if (err) {
-                throw err;
-              } else if (options.skip === 0 && _.isEmpty(followers)) {
-                cb(null);
-              } else if (options.skip > 0 && _.isEmpty(followers)) {
-                cb(null);
-              } else {
-                result.result = followers;
-                cb(null);
-              }
-            });
-          },
-        ],
-        function (err) {
-          if (err) {
-            throw err;
-          } else if (options.skip === 0 && _.isEmpty(result.result)) {
-            response.setData(AppCode.NotFound, result);
-            response.send(res);
-          } else if (options.skip > 0 && _.isEmpty(result.result)) {
-            response.setData(AppCode.NotFound, result);
-            response.send(res);
-          } else {
-            response.setData(AppCode.Success, result);
-            response.send(res);
-          }
-        }
-      );
-    } catch (exception) {
-      response.setError(AppCode.InternalServerError);
-      response.send(res);
-    }
-  });
 };
 
 
@@ -386,7 +230,7 @@ blockUserCtrl.unblockUser = (req, res) => {
   try {
     BlockUserModel.findOne(
       {
-        userId: ObjectID(req.body._id),
+        userId: ObjectID(req.auth._id),
         blockedUserId: ObjectID(req.body.blockedUserId),
       },
       (err, blockedUserFind) => {
@@ -395,7 +239,7 @@ blockUserCtrl.unblockUser = (req, res) => {
         } else {
           BlockUserModel.remove(
             {
-              userId: ObjectID(req.body._id),
+              userId: ObjectID(req.auth._id),
               blockedUserId: ObjectID(req.body.blockedUserId),
             },
             (err, newId) => {
@@ -403,95 +247,91 @@ blockUserCtrl.unblockUser = (req, res) => {
                 throw err;
               } else {
 
-                //block array save in user table 
-                UserModel.findOne(
-                  {
-                    _id: ObjectID(req.body._id),
-                  },
-                  (err, userfind) => {
-                    if (err) {
-                      console.log(err);
-                      throw err;
-                    } else {
-                      if (userfind == null) {
-                        AppCode.Fail.error = "No record found";
-                        response.setError(AppCode.Fail);
-                        response.send(res);
-                      } else {
-                        // console.log("................",userfind)
+                //block array remove
+                // UserModel.findOne(
+                //   {
+                //     _id: ObjectID(req.body._id),
+                //   },
+                //   (err, userfind) => {
+                //     if (err) {
+                //       console.log(err);
+                //       throw err;
+                //     } else {
+                //       if (userfind == null) {
+                //         AppCode.Fail.error = "No record found";
+                //         response.setError(AppCode.Fail);
+                //         response.send(res);
+                //       } else {
+                //         // console.log("................",userfind)
 
 
-                        let updateDataQuery = {};
-                        //  console.log("................",userfind.blockUser)
-                        let users = []
-                        let Users = []
-                        users = userfind.blockUser
-                        Users = userfind.blockUser
+                //         let updateDataQuery = {};
+                //         //  console.log("................",userfind.blockUser)
+                //         let users = []
+                //         let Users = []
+                //         users = userfind.blockUser
+                //         Users = userfind.blockUser
 
-                        console.log("usersssssssss", users);
+                //         console.log("usersssssssss", users);
 
-                        for (let i = 0; i < Users.length; i++) {
+                //         for (let i = 0; i < Users.length; i++) {
 
-                          if ((req.body.blockedUserId).toString() == (Users[i]).toString()) {
-                            console.log("disconneceted user...................", Users[i])
-
-
-                            console.log(".....if......")
-
-                            users.splice(i, 1)
+                //           if ((req.body.blockedUserId).toString() == (Users[i]).toString()) {
+                //             console.log("disconneceted user...................", Users[i])
 
 
-                            updateDataQuery.blockUser = users
+                //             console.log(".....if......")
 
-                            // if ((Users.length - 1) == i) {
-                              UserModel.update({ _id: ObjectID(req.body._id) }, updateDataQuery, function (err, roleUpdate) {
-                                if (err) {
-                                  console.log("...........", err);
-                                  response.setError(AppCode.Fail);
-                                } else {
-
-                                  console.log(")))))))))", updateDataQuery);
-                                  response.setData(AppCode.Success);
-                                  response.send(res);
-                                }
-                              });
-                            // }
-
-                          }
-                          else {
-                            console.log("...............else");
-                          }
-
-                        }
+                //             users.splice(i, 1)
 
 
-                      
+                //             updateDataQuery.blockUser = users
 
-                      //console.log("user disconnected", users);
-                      // console.log("user connected after disconnecting......", users);
+                //             // if ((Users.length - 1) == i) {
+                //               UserModel.update({ _id: ObjectID(req.body._id) }, updateDataQuery, function (err, roleUpdate) {
+                //                 if (err) {
+                //                   console.log("...........", err);
+                //                   response.setError(AppCode.Fail);
+                //                 } else {
 
-                      updateDataQuery.blockUser = users
+                //                   console.log(")))))))))", updateDataQuery);
+                //                   response.setData(AppCode.Success);
+                //                   response.send(res);
+                //                 }
+                //               });
+                //             // }
+
+                //           }
+                //           else {
+                //             console.log("...............else");
+                //           }
+
+                //         }
 
 
 
+
+                //       //console.log("user disconnected", users);
+                //       // console.log("user connected after disconnecting......", users);
+
+                //       updateDataQuery.blockUser = users
 
 
 
 
 
-                    }
-                  }
-                  }
-                );
 
 
 
+                //     }
+                //   }
+                //   }
+                // );
 
-
-          //response.setData(AppCode.Success);
-          // response.send(res);
-        }
-      }
+                response.setData(AppCode.Success);
+                response.send(res);
+              }
+            }
     );
   }
       }
